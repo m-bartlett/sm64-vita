0db4cf2cac07a9e4084b73c9480e0cca16644140
M B <85039141+m-bartlett@users.noreply.github.com>
2022-07-17 12:38:09 -0600

    Preferred controls remap
    Vita L -> N64 Z
    Vita R -> N64 L (center camera)
    Vita △ -> N64 R (change camera view)

--- src/pc/controller/controller_vita.c
+++ src/pc/controller/controller_vita.c
@@ -16,26 +16,17 @@ static void vita_read(OSContPad *pad) {
     SceCtrlData ctrl;
     sceCtrlPeekBufferPositive(0, &ctrl, 1);
 
-    if (ctrl.buttons & SCE_CTRL_UP)
-        pad->button |= U_JPAD;
-    if (ctrl.buttons & SCE_CTRL_DOWN)
-        pad->button |= D_JPAD;
-    if (ctrl.buttons & SCE_CTRL_LEFT)
-        pad->button |= L_JPAD;
-    if (ctrl.buttons & SCE_CTRL_RIGHT)
-        pad->button |= R_JPAD;
-    if (ctrl.buttons & SCE_CTRL_START)
-        pad->button |= START_BUTTON;
-    if (ctrl.buttons & SCE_CTRL_LTRIGGER)
-        pad->button |= Z_TRIG;
-    if (ctrl.buttons & SCE_CTRL_RTRIGGER)
-        pad->button |= R_TRIG;
-    if (ctrl.buttons & SCE_CTRL_CROSS)
-        pad->button |= A_BUTTON;
-    if (ctrl.buttons & SCE_CTRL_CIRCLE)
-        pad->button |= B_BUTTON;
-    if (ctrl.buttons & SCE_CTRL_SQUARE)
-        pad->button |= B_BUTTON;
+    if (ctrl.buttons & SCE_CTRL_UP)       pad->button |= U_JPAD;
+    if (ctrl.buttons & SCE_CTRL_DOWN)     pad->button |= D_JPAD;
+    if (ctrl.buttons & SCE_CTRL_LEFT)     pad->button |= L_JPAD;
+    if (ctrl.buttons & SCE_CTRL_RIGHT)    pad->button |= R_JPAD;
+    if (ctrl.buttons & SCE_CTRL_START)    pad->button |= START_BUTTON;
+    if (ctrl.buttons & SCE_CTRL_LTRIGGER) pad->button |= Z_TRIG;
+    if (ctrl.buttons & SCE_CTRL_RTRIGGER) pad->button |= L_TRIG;
+    if (ctrl.buttons & SCE_CTRL_CROSS)    pad->button |= A_BUTTON;
+    if (ctrl.buttons & SCE_CTRL_CIRCLE)   pad->button |= B_BUTTON;
+    if (ctrl.buttons & SCE_CTRL_SQUARE)   pad->button |= B_BUTTON;
+    if (ctrl.buttons & SCE_CTRL_TRIANGLE) pad->button |= R_TRIG;
 
     pad->stick_x = (s8)((s32) ctrl.lx - 128);
     pad->stick_y = (s8)(-((s32) ctrl.ly - 127));
